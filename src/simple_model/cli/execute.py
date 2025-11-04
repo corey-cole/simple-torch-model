05b266e3b03de27597085a34c62d6621be162c4c
Corey Cole <3700879+corey-cole@users.noreply.github.com>
2025-11-03 21:10:04 -0700

    Put model load/execution into class

--- src/simple_model/cli/execute.py
+++ src/simple_model/cli/execute.py
@@ -6,6 +6,7 @@ import torch
 from executorch.runtime import Runtime
 
 from simple_model.cli.common import ExportFormat
+from simple_model.runner import ModelRunner
 
 
 @click.command(context_settings={"show_default": True})
@@ -24,16 +25,14 @@ from simple_model.cli.common import ExportFormat
 def main(format: ExportFormat, input_path: str) -> None:
     print(f"Loading model from {input_path} in {format.value} format.")
 
+    runner = ModelRunner()
     match format:
-        # TODO: Move this out of the CLI module
         case ExportFormat.XNNPACK:
-            runtime = Runtime.get()
-            input_tensor = torch.randn(1, 10)
-            program = runtime.load_program(input_path)
-            method = program.load_method("forward")
-            if method is None:
-                raise RuntimeError("Failed to load 'forward' method from program.")
-            output = method.execute([input_tensor])
-            print(f"Model output: {output}")
+            runner.run_xnnpack_model(input_path)
+        case ExportFormat.ONNX:
+            runner.run_onnx_model(input_path)
+        case ExportFormat.TORCH_EXPORT:
+            runner.run_torch_export_model(input_path)
         case _:
             raise ValueError(f"Unsupported export format: {format}")
+
--- src/simple_model/runner.py
+++ src/simple_model/runner.py
@@ -1,27 +1,47 @@
 from __future__ import annotations
 
-import click
+import onnxruntime as ort
 import torch
 
 from executorch.runtime import Runtime
 
+
 from simple_model.cli.common import ExportFormat
 
+# This class runs the exported SimpleModel or ConditionalModel.  It doesn't take input
+# and instead uses a random tensor for demonstration purposes.
+class ModelRunner:
+    def __init__(self):
+        pass
 
-def main(format: ExportFormat, input_path: str) -> None:
-    print(f"Loading model from {input_path} in {format.value} format.")
+    def run_xnnpack_model(self, input_path: str) -> None:
+        runtime = Runtime.get()
+        input_tensor = torch.randn(1, 10)
+        program = runtime.load_program(input_path)
+        method = program.load_method("forward")
+        if method is None:
+            raise RuntimeError("Failed to load 'forward' method from program.")
+        output = method.execute([input_tensor])
+        print(f"Model output: {output}")
+        return
+    
+    def run_onnx_model(self, input_path: str) -> None:
+        input_tensor = torch.randn(1, 10).numpy()
+        session = ort.InferenceSession(input_path)
+        input_name = session.get_inputs()[0].name
+        outputs = session.run(None, {input_name: input_tensor})
+        print(f"Model output: {outputs[0]}")
+        return
+    
+    def run_torch_export_model(self, input_path: str) -> None:
+        loaded_model = torch.export.load(input_path)
+        input_tensor = torch.randn(1, 10)
+        module = loaded_model.module()
+        if module is None:
+            raise RuntimeError("Failed to get module from loaded model.")
+        if not hasattr(module, "forward") or not callable(module.forward):
+            raise RuntimeError("Loaded module does not have a 'forward' method.")
+        output = module.forward(input_tensor)
+        print(f"Model output: {output}")
+        return
 
-    match format:
-        # TODO: Move this out of the CLI module
-        # TODO: Execute the same input_tensor against the original model for comparison
-        case ExportFormat.XNNPACK:
-            runtime = Runtime.get()
-            input_tensor = torch.randn(1, 10)
-            program = runtime.load_program(input_path)
-            method = program.load_method("forward")
-            if method is None:
-                raise RuntimeError("Failed to load 'forward' method from program.")
-            output = method.execute([input_tensor])
-            print(f"Model output: {output}")
-        case _:
-            raise ValueError(f"Unsupported export format: {format}")
